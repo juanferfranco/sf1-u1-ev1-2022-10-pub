efb79cdace807ab10cd6e8a2061939d31255ee00
Juan Franco <juanferfranco@gmail.com>
2022-02-21 07:39:41 -0500

    init repo

---
+++ bomb/bomb.ino
@@ -0,0 +1,19 @@
+#include "SSD1306Wire.h"
+
+#define BOMB_OUT 25
+#define LED_COUNT 26
+#define UP_BTN 13
+#define DOWN_BTN 32
+#define ARM_BTN 33
+
+// Selecciona uno según tu display.
+//SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_128_32);
+SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_64_48); 
+
+void setup() {
+
+}
+
+void loop() {
+
+}
